32102d984b7afa712f02e9f1245326a5fb459e72
ssamilg <samil-98@hotmail.com>
2025-04-05 17:19:52 +0300

    chore: add constant horses

---
+++ src/constants/horses.js
@@ -0,0 +1,147 @@
+/**
+ * List of 20 horses with BJK footballer names, colors, and condition scores.
+ * Each horse has a unique Tailwind color and a condition score between 30-100.
+ */
+
+export const HORSES = [
+  {
+    id: 1,
+    name: 'Sergen Yalçın',
+    color: 'bg-black',
+    textColor: 'text-white',
+    condition: 95
+  },
+  {
+    id: 2,
+    name: 'Metin Tekin',
+    color: 'bg-gray-800',
+    textColor: 'text-white',
+    condition: 90
+  },
+  {
+    id: 3,
+    name: 'Feyyaz Uçar',
+    color: 'bg-white',
+    textColor: 'text-black',
+    condition: 88
+  },
+  {
+    id: 4,
+    name: 'Ali Gültiken',
+    color: 'bg-gray-400',
+    textColor: 'text-black',
+    condition: 85
+  },
+  {
+    id: 5,
+    name: 'Pascal Nouma',
+    color: 'bg-blue-600',
+    textColor: 'text-white',
+    condition: 79
+  },
+  {
+    id: 6,
+    name: 'Mario Gomez',
+    color: 'bg-red-600',
+    textColor: 'text-white',
+    condition: 86
+  },
+  {
+    id: 7,
+    name: 'Ricardo Quaresma',
+    color: 'bg-purple-600',
+    textColor: 'text-white',
+    condition: 92
+  },
+  {
+    id: 8,
+    name: 'Vincent Aboubakar',
+    color: 'bg-yellow-500',
+    textColor: 'text-black',
+    condition: 83
+  },
+  {
+    id: 9,
+    name: 'Demba Ba',
+    color: 'bg-green-600',
+    textColor: 'text-white',
+    condition: 82
+  },
+  {
+    id: 10,
+    name: 'İlhan Mansız',
+    color: 'bg-pink-500',
+    textColor: 'text-white',
+    condition: 87
+  },
+  {
+    id: 11,
+    name: 'Recep Çetin',
+    color: 'bg-indigo-600',
+    textColor: 'text-white',
+    condition: 78
+  },
+  {
+    id: 12,
+    name: 'Samet Aybaba',
+    color: 'bg-teal-600',
+    textColor: 'text-white',
+    condition: 89
+  },
+  {
+    id: 13,
+    name: 'Nihat Kahveci',
+    color: 'bg-amber-500',
+    textColor: 'text-black',
+    condition: 82
+  },
+  {
+    id: 14,
+    name: 'Gökhan Töre',
+    color: 'bg-lime-500',
+    textColor: 'text-black',
+    condition: 73
+  },
+  {
+    id: 15,
+    name: 'Necip Uysal',
+    color: 'bg-orange-600',
+    textColor: 'text-white',
+    condition: 77
+  },
+  {
+    id: 16,
+    name: 'Daniel Amokachi',
+    color: 'bg-cyan-500',
+    textColor: 'text-black',
+    condition: 81
+  },
+  {
+    id: 17,
+    name: 'Pepe',
+    color: 'bg-fuchsia-600',
+    textColor: 'text-white',
+    condition: 84
+  },
+  {
+    id: 18,
+    name: 'Atiba Hutchinson',
+    color: 'bg-rose-600',
+    textColor: 'text-white',
+    condition: 75
+  },
+  {
+    id: 19,
+    name: 'Oğuzhan Özyakup',
+    color: 'bg-emerald-500',
+    textColor: 'text-white',
+    condition: 69
+  },
+  {
+    id: 20,
+    name: 'Rafa Silva',
+    color: 'bg-sky-500',
+    textColor: 'text-white',
+    condition: 88
+  }
+];
