8e6c870672a8f35fed4e7339caacb7a2dff25d5f
ssamilg <samil-98@hotmail.com>
2025-04-05 17:18:56 +0300

    chore: add project files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    <div class="w-1/2 h-1/2 bg-red-500">
+      LEL
+    </div>
+  </div>
+</template>
